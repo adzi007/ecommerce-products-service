da8249cfd769cdb45adf06a2acbe11920eeb0b70
Ahmad Fauzi <adzi.fauziahmad@gmail.com>
2025-05-04 18:47:59 +0700

    apply rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { CategoryModule } from './category/category.module';
 import { RedisModule } from './redis/redis.module';
 // import { PrometheusModule } from './prometheus/prometheus.module';
 import { PrometheusController } from './prometheus/prometheus.controller';
-import { MetricsMiddleware } from './common/middleware/metrics.middleware';
+// import { MetricsMiddleware } from './common/middleware/metrics.middleware';
 import { PrometheusService } from './prometheus/prometheus.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -13,9 +15,23 @@ import { PrometheusService } from './prometheus/prometheus.service';
     ProductsModule, 
     CategoryModule,
     // PrometheusModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 15000, // 15 seconds
+          limit: 10,
+        },
+      ],
+    }),
   ],
   controllers: [PrometheusController],
-  providers:[PrometheusService],
+  providers:[
+    PrometheusService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    }
+  ],
 })
 export class AppModule {}
 // export class AppModule implements NestModule {
